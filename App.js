c0957a87c52aae85fd399a55accd7a0833f7ca31
sagar <sagarchauhan265@gmail.com>
2023-09-05 05:23:35 +0530

    update camera

--- App.js
+++ App.js
@@ -2,16 +2,12 @@ import { View, Text,useColorScheme} from 'react-native'
 import React from 'react'
 import TabViewDemo from './TabViewDemo';
 import QrCode from './src/QrCode'
+import Demo from './src/Demo';
+import Generator from './src/Generator';
 export default function App() {
 
 
   return (
-<<<<<<< HEAD
-    <QrCode />
-=======
-    <View>
-      <Text>hello world!</Text>
-    </View>
->>>>>>> 9a0eff0bff8c432f1d5b465c50010b7e7ec39c63
+    <Demo />
   )
 }
---
+++ src/Generator.js
@@ -0,0 +1,39 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import { View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
+export default function Generator() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ])
+    }, [])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+    return (
+        <View style={{flex:1,backgroundColor:'#ffffff'}}>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+        </View>
+
+    )
+}
